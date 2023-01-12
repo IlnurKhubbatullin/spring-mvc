4f10f8fbac0e4908bea4ce1b91a1e3c0a4f592b2
Ильнур <ilnurka_16@mail.ru>
2023-01-12 20:40:20 +0300

    Калькулятор

--- src/main/java/ru/maxima/springmvc/controllers/CalculateController.java
+++ src/main/java/ru/maxima/springmvc/controllers/CalculateController.java
@@ -13,8 +13,23 @@ public class CalculateController {
                           @RequestParam(value = "num2" , required = false) double num2,
                           @RequestParam(value = "operation" , required = false) String operation,
                           Model model) {
+        double result = 0;
+        switch (operation) {
+            case "addition":
+                result = num1 + num2;
+                break;
+            case "subtraction":
+                result = num1 - num2;
+                break;
+            case "multiplication":
+                result = num1 * num2;
+                break;
+            case "division":
+                result = (double) num1 / num2;
+                break;
+        }
+        model.addAttribute("result", "Answer" + " " + result);
 
-        model.addAttribute("result", "Kolesnikov Oleg");
         return "calculate";
     }
 
